ff481eb5b206f382cbd31d5180ec43ef26f85b27
ivanchotom <46633666+ivanchotom@users.noreply.github.com>
2020-12-11 05:19:48 +0000

    Started Mesh class (brocken)

---
+++ Assignment/Meshtest.h
@@ -0,0 +1,65 @@
+#pragma once
+#include "glad/glad.h"
+
+#include "glm/glm/glm.hpp"
+#include "glm/glm/gtc/matrix_transform.hpp"
+
+#include "Shadertest.h"
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <vector>
+
+
+struct Vertex {
+	// position
+	glm::vec3 Position;
+	// normal
+	glm::vec3 Normal;
+	// texCoords
+	glm::vec2 TexCoords;
+	// tangent
+	glm::vec3 Tangent;
+	// bitangent
+	glm::vec3 Bitangent;
+};
+
+struct Texture {
+	unsigned int id;
+	std::string type;
+	std::string path;
+
+	std::string GetPath() { return path; };
+	std::string GetType() { return type; };
+	unsigned int GetId() { return id; };
+
+};
+
+class Mesh {
+public:
+
+
+
+	// constructor
+	Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures);
+
+
+	// render the mesh
+	void Draw(std::shared_ptr<Shader> shader);
+
+
+private:
+	/*  Render data  */
+
+	std::vector<Vertex> vertices;
+	std::vector<unsigned int> indices;
+	std::vector<Texture> textures;
+	unsigned int VAO;
+	unsigned int VBO, EBO;
+
+	/*  Functions    */
+	// initializes all the buffer objects/arrays
+	void setupMesh();
+};
---
+++ Assignment/Modeltest.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "glm/glm/glm.hpp"
+#include "glm/glm/gtc/matrix_transform.hpp"
+#include "stb_image.h"
+#include "Dependencies/assimp/include/assimp/Importer.hpp"
+#include "Dependencies/assimp/include/assimp/scene.h"
+#include "Dependencies/assimp/include/assimp/postprocess.h"
+
+#include "Mesh.h"
+#include "Shadertest.h"
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <map>
+#include <vector>
+
+class Model
+{
+public:
+	/*  Model Data */
+
+
+
+	// constructor, expects a filepath to a 3D model.
+	Model(std::string const& path, bool gamma = false);
+
+	// draws the model, and thus all its meshes
+	void Draw(std::shared_ptr<Shader> shader);
+
+
+
+private:
+
+	void loadModel(std::string const& path);
+
+	// processes a node in a recursive fashion. Processes each individual mesh located at the node and repeats this process on its children nodes (if any).
+	void processNode(aiNode* node, const aiScene* scene);
+
+	Mesh processMesh(aiMesh* mesh, const aiScene* scene);
+
+
+	// checks all material textures of a given type and loads the textures if they're not loaded yet.
+	// the required info is returned as a Texture struct.
+	std::vector<Texture> loadMaterialTextures(aiMaterial* mat, aiTextureType type, std::string typeName);
+	/*  Model Data */
+	std::vector<Texture> textures_loaded;	// stores all the textures loaded so far, optimization to make sure textures aren't loaded more than once.
+	std::vector<Mesh> meshes;
+	std::string directory;
+	bool gammaCorrection;
+
+
+};
